0a2a83e460af461f6a153c9b7bcb80ff3c2825c6
Andrey Malakhov <a.malakhov@mobidev.biz>
2014-01-24 17:36:04 +0200

    Add bundle log sample

--- android_framework_samples/src/blak/android/example/LogSample.java
+++ android_framework_samples/src/blak/android/example/LogSample.java
@@ -1,16 +1,24 @@
 package blak.android.example;
 
-import blak.android.utils.MyLog;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import blak.android.utils.MyLog;
 
 public class LogSample extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Bundle bundle = new Bundle();
+        bundle.putString("name", "trinity");
+        Intent intent = new Intent();
+        intent.putExtra("BUNDLE", bundle);
+        bundle = intent.getParcelableExtra("BUNDLE");
+
         MyLog.v("asd", null, 3);
         MyLog.vn("asd", null, 3);
+        MyLog.vn(bundle);
     }
 }
